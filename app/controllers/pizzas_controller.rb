b722eef79a4a6323e6f71dc54282689d0243e347
estherkang14 <estherkang14@gmail.com>
2020-07-01 11:13:56 -0400

    finished.. forgot to commit often but will go back to add/fix stuff

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,47 @@
 class PizzasController < ApplicationController
+
+  before_action :find_pizza, only: [:show, :destroy]
+
   def index
     @pizzas = Pizza.all
+    #byebug
   end
+
+  def show 
+    
+  end 
+
+  def new 
+    @pizza = Pizza.new
+  end 
+
+  def create
+    @pizza = Pizza.new(pizza_params)
+      if @pizza.save 
+        redirect_to pizza_path(@pizza.id)
+      else 
+        render :new
+      end 
+  end 
+
+  def edit
+  end 
+
+  def update
+  end
+
+  def destroy
+    @pizza.destroy
+    redirect_to pizzas_path
+  end 
+
+  private 
+
+  def find_pizza
+    @pizza = Pizza.find(params[:id])
+  end 
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_ids)
+  end 
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,10 @@
 class Pizza < ApplicationRecord
+    has_many :restaurant_pizzas
+    has_many :restaurants, through: :restaurant_pizzas
+    validates :name, presence: true, uniqueness: true 
+    validates :ingredients, presence: true 
+
+    def pizza_ings
+        self.ingredients.split(",")
+    end 
 end
