27e5998b6b773d101832ba9cbdedd6203ccaecfe
andy-aguilar <aaguil3@gmail.com>
2020-07-01 11:11:13 -0400

    I should have committed way more often, but I'm done.

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,9 @@
 class Pizza < ApplicationRecord
+    has_many :restaurant_pizzas
+    has_many :restaurants, through: :restaurant_pizzas
+
+    validates :name, presence:true, uniqueness: { 
+        case_sensitive: false,
+        message: "be original, %{value} already exists." }
+    validates :ingredients, presence:true
 end
